935f57c2a9d944ef13292e5ec7bcd62d4dcaccf2
Robert Philipp <rob.philipp@gmail.com>
2018-09-22 17:58:40 -0400

    generalized the plotSeries function combining plotWeightSeries and plotMembranePotentialSeries

--- src/plots/plots.jl
+++ src/plots/plots.jl
@@ -42,26 +42,77 @@ function addSeriesToPlot(x::Array{Float64,1}, y::Array{Float64,1}, p::Plots.Plot
     plot!(p, x, y, label=name, hover=hoverLabels(x, y, name))
 end
 
-# """
-# plotMembranePotential(dataFrame::DataFrame, series::Int)
+"""
+plotSeries(
+    dataFrames::Dict{Integer, DataFrame}, 
+    plotFunction::Function;
+    numCols::Int = 2,
+    xAxisLabel::String = "t (ms)",
+    yAxisLabel::String = "",
+    title::String = ""
+)
 
-# Plots the membrane potential for the optionally specified series
+Creates a page of connection-weight time-series subplots, one for each run (series).
+Provides a convenient way to compare the weight time-series from a series of runs.
 
-# # Arguments
-# * `dataFrame::DataFrame`: The data frame holding the membrane potential events
-# * `series::Int = 1`: The optional series number
-# """
-# function plotMembranePotential(dataFrame::DataFrame, series::Int=-1)
-#     lines = scatter(dataFrame, group=:neuron_id, x=:signal_time, y=:potential)
-#     plot(lines,
-#         Layout(title="Membrane Potential$(series >= 0 ? " - series: $series" : "")",
-#             xaxis=attr(title="t (ms)", showgrid=true),
-#             yaxis=attr(title="U (mV)", showgrid=true)
-#         )
-#     )
-# end
+# Arguments
+* `dataFrames::Dict{Integer, DataFrame}`: A dictionary holding the series-number -> DataFrame
+* `plotFunction::Function`: A function that accepts a DataFrame, the series index, the
+        label for the x-axis, the label for the y-axis, and the plot title
+* `numCols::Int`: Optional name parameter the specifies the number of subplot columns. The
+        default value is 2 columns.
+* `xAxisLabel::String`: Optional label for the x-axis. Default value is "t (ms)".
+* `yAxisLabel::String`: Optional label for the y-axis. Default value is an empty string.
+* `title::String`: Optional title for the plot
+"""
+function plotSeries(
+    dataFrames::Dict{Integer, DataFrame},
+    plotFunction::Function;
+    numCols::Int = 2,
+    xAxisLabel::String = "t (ms)",
+    yAxisLabel::String = "",
+    title::String = ""
+)
 
+    numRows::Int = ceil(length(dataFrames) / numCols)
 
-function plotSeriesFrom(series::Dict{Integer, DataFrame}, plotFunction::Function)
-    [plotFunction(entry[2], entry[1]) for entry in series]
+    # function to update the name of the series by prepending the series number
+    updateName = (scat, seriesNumber::Int) -> begin
+        scat[:name] = "s$(seriesNumber): $(scat[:name])"
+        scat
+    end
+
+    numTracesPerPlot::Int = 1
+    plots = []
+    for entry in sort(collect(dataFrames), by = key -> key[1])
+        index = entry[1]
+        dataFrame = entry[2]
+
+        # create the axis identifiers. The first plot gets the largest y-axis id
+        # so that the first dataframe in the series is the first plot at the top
+        # left-hand side of the page
+        scatters = plotFunction(
+            dataFrame,
+            series = index,
+            xaxis = index > length(dataFrames) - numCols ? xAxisLabel : "", 
+            yaxis = index % numCols == 1 ? yAxisLabel : "",
+            title = "$(index >= 0 ? "($index)" : title)"
+        )
+
+        # keep track of the number of traces per subplot for selecting col
+        numTracesPerPlot = max(numTracesPerPlot, length(scatters))
+
+        # add the traces from the scatter plot
+        push!(plots, scatters)
+    end
+
+    # create and return the plot
+    # plot(vcat(plots...), style=style)
+    plot(
+        plots...,
+        layout=(numRows, numCols),
+        legend=false,
+        size=(600 * numCols, 300 * numRows)
+    )
 end
+
